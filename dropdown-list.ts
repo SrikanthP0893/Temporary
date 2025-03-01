ca658454e2153defa60c0fbf98062ffe67be1323
SrikanthP0893 <132909535+SrikanthP0893@users.noreply.github.com>
2025-03-01 14:48:59 +0530

    Add files via upload

---
+++ dropdown-list.ts
@@ -0,0 +1,42 @@
+export const DropdownList = [
+  {
+    image:
+      'https://prweb-assets-prod.s3.amazonaws.com/default-avatars/Olivia+Rhye.jpg',
+    value: 'Perry M',
+    shortValue: 'Perry',
+    displayId: 'perry12345',
+    _id: '11',
+  },
+  {
+    image:
+      'https://prweb-assets-prod.s3.amazonaws.com/default-avatars/Olivia+Rhye.png',
+    value: 'Glen M',
+    shortValue: 'Glen',
+    displayId: 'glen1234',
+    _id: '12',
+  },
+  {
+    image:
+      'https://prweb-assets-prod.s3.amazonaws.com/default-icons/koala.svg',
+    value: 'Venkat Katta',
+    shortValue: 'Venkat',
+    displayId: 'venkat123',
+    _id: '13',
+  },
+  {
+    image:
+      'https://prweb-assets-prod.s3.amazonaws.com/default-icons/penguin.svg',
+    value: 'Robert BIgini',
+    shortValue: 'Robert',
+    displayId: 'rob1234',
+    _id: '14',
+  },
+  {
+    image:
+      'https://prweb-assets-prod.s3.amazonaws.com/default-avatars/Olivia+Rhye.jpg',
+    value: 'Jana De bruyn',
+    shortValue: 'Jana',
+    displayId: 'jana1234',
+    _id: '15',
+  },
+];
